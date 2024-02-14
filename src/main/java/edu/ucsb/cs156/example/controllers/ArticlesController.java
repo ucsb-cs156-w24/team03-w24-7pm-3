a7c9f061f54c2f8fb7c4e48e0378f435b3435127
Jason Gu <1057337859@qq.com>
2024-02-13 18:02:44 -0800

    copied CRUD from team02

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -2,11 +2,15 @@ package edu.ucsb.cs156.example.controllers;
 
 import java.time.LocalDateTime;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,5 +86,26 @@ public class ArticlesController extends ApiController{
 
                 articlesRepository.delete(article);
         return genericMessage("Article with id %s deleted".formatted(id));
+    }
+
+    @Operation(summary= "Update a single article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public Articles updateArticles(
+            @Parameter(name="id") @RequestParam Long id,
+            @RequestBody @Valid Articles incoming) {
+
+        Articles articles = articlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Articles.class, id));
+
+            articles.setTitle(incoming.getTitle());
+            articles.setUrl(incoming.getUrl());
+            articles.setExplanation(incoming.getExplanation());
+            articles.setEmail(incoming.getEmail());
+            articles.setDateAdded(incoming.getDateAdded());
+
+        articlesRepository.save(articles);
+
+        return articles;
     }
 }
--- src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
@@ -254,4 +254,86 @@ public class ArticlesControllerTests extends ControllerTestCase{
             assertEquals("Articles with id 15 not found", json.get("message"));
     }
     
+    // Tests for PUT /api/articles?id=... 
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void admin_can_edit_an_existing_article() throws Exception {
+            // arrange
+
+            LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+            LocalDateTime ldt2 = LocalDateTime.parse("2023-01-03T00:00:00");
+
+            Articles articlesOrig = Articles.builder()
+                    .title("Using testing-playground with React Testing Library")
+                    .url("https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7")
+                    .explanation("Helpful when we get to front end development")
+                    .email("phtcon@ucsb.edu")
+                    .dateAdded(ldt1)
+                    .build();
+
+            Articles articlesEdited = Articles.builder()
+                    .title("Handy Spring Utility Classes")
+                    .url("https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09")
+                    .explanation("A lot of really useful classes are built into Spring")
+                    .email("yisheng@ucsb.edu")
+                    .dateAdded(ldt2)
+                    .build();
+
+            String requestBody = mapper.writeValueAsString(articlesEdited);
+
+            when(articlesRepository.findById(eq(67L))).thenReturn(Optional.of(articlesOrig));
+
+            // act
+            MvcResult response = mockMvc.perform(
+                            put("/api/articles?id=67")
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .characterEncoding("utf-8")
+                                            .content(requestBody)
+                                            .with(csrf()))
+                            .andExpect(status().isOk()).andReturn();
+
+            // assert
+            verify(articlesRepository, times(1)).findById(67L);
+            verify(articlesRepository, times(1)).save(articlesEdited); // should be saved with correct user
+            String responseString = response.getResponse().getContentAsString();
+            assertEquals(requestBody, responseString);
+    }
+
+    
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void admin_cannot_edit_articles_that_does_not_exist() throws Exception {
+            // arrange
+
+            LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+            Articles articlesEdited = Articles.builder()
+                    .title("Handy Spring Utility Classes")
+                    .url("https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09")
+                    .explanation("A lot of really useful classes are built into Spring")
+                    .email("phtcon@ucsb.edu")
+                    .dateAdded(ldt1)
+                    .build();
+
+            String requestBody = mapper.writeValueAsString(articlesEdited);
+
+            when(articlesRepository.findById(eq(67L))).thenReturn(Optional.empty());
+
+            // act
+            MvcResult response = mockMvc.perform(
+                            put("/api/articles?id=67")
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .characterEncoding("utf-8")
+                                            .content(requestBody)
+                                            .with(csrf()))
+                            .andExpect(status().isNotFound()).andReturn();
+
+            // assert
+            verify(articlesRepository, times(1)).findById(67L);
+            Map<String, Object> json = responseToJson(response);
+            assertEquals("Articles with id 67 not found", json.get("message"));
+
+    }
+    
 }
