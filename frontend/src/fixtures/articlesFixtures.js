9f5996a021b81c30c6f39f33d77407762011cf66
Jason Gu <1057337859@qq.com>
2024-02-14 16:37:09 -0800

    add article fixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,47 @@
+const articlesFixtures = {
+    oneArticle:
+    [
+      {
+       "id": 1,
+        "title": "Article1",
+        "url": "http://article1.com",
+        "explanation":"explanation1" ,
+        "email":"email1@gmail.com",
+        "localDateTime": "2022-01-02T12:00:00"
+
+      }
+    ],
+
+    threeArticles:
+    [
+        {
+            "id": 2,
+            "title": "Article2",
+            "url": "http://article2.com",
+            "explanation":"explanation2" ,
+            "email":"email2@gmail.com",
+            "localDateTime": "2022-02-02T12:00:00"
+        },
+
+        {
+            "id": 3,
+            "title": "Article3",
+            "url": "http://article3.com",
+            "explanation":"explanation3" ,
+            "email":"email3@gmail.com",
+            "localDateTime": "2022-03-02T12:00:00"
+        },
+
+        {
+            "id": 4,
+            "title": "Article4",
+            "url": "http://article4.com",
+            "explanation":"explanation4" ,
+            "email":"email4@gmail.com",
+            "localDateTime": "2022-04-02T12:00:00"
+        },
+        
+    ]
+};
+
+export { articlesFixtures };
