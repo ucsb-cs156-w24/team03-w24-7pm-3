7d9945645a4eac5fce8dbf6c7a9aeb137eec8ad6
Shamita Gurusu <shamita@ucsb.edu>
2024-02-18 20:04:22 -0800

    index page implementation

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
@@ -1,15 +1,45 @@
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend';
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestTable from 'main/components/RecommendationRequest/RecommendationRequestTable';
+import { useCurrentUser , hasRole} from 'main/utils/currentUser'
+import { Button } from 'react-bootstrap';
 
 export default function RecommendationRequestIndexPage() {
 
-  // Stryker disable all : placeholder for future implementation
-  return (
-    <BasicLayout>
-      <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p><a href="/recommendationrequest/create">Create</a></p>
-        <p><a href="recommendationrequest/edit/1">Edit</a></p>
-      </div>
-    </BasicLayout>
-  )
+    const currentUser = useCurrentUser();
+
+    const { data: recommendationRequest, error: _error, status: _status } =
+        useBackend(
+            // Stryker disable next-line all : don't test internal caching of React Query
+            ["/api/recommendationrequests/all"],
+            { method: "GET", url: "/api/recommendationrequests/all" },
+            // Stryker disable next-line all : don't test default value of empty list
+            []
+        );
+
+    const createButton = () => {
+        if (hasRole(currentUser, "ROLE_ADMIN")) {
+            return (
+                <Button
+                    variant="primary"
+                    href="/recommendationrequest/create"
+                    style={{ float: "right" }}
+                >
+                    Create Recommendation Request
+                </Button>
+            )
+        } 
+    }
+
+    return (
+        <BasicLayout>
+            <div className="pt-2">
+                {createButton()}
+                <h1>RecommendationRequest</h1>
+                <RecommendationRequestTable recommendationRequest={recommendationRequest} currentUser={currentUser} />
+            </div>
+        </BasicLayout>
+    );
 }
